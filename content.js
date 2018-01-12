2c205f8caebfdcaccbbb9a8e9add5e6fd8ba24b1
jstaab <jasperstaab@gmail.com>
2018-01-12 12:15:38 -1000

    Simplify Selectors (#19)
    * Simply selectors by using querySelectorAll. No need to differentiate between different selector types.
    * Eliminate the need to have childElement specifications.
    * Remove unused attribute settings that control unredaction. Instead just remove attributes entirely when in unredacted state
    * Listen for attribute changes in the DOM, not just childList changes

--- content.js
+++ content.js
@@ -4,96 +4,61 @@ chrome.runtime.onMessage.addListener(
   function(request) {
     if (request.action === 'icon_click') {
       hide = !hide
-      redact()
+      parsePage()
     }
   }
 )
 
-const redactors = Object.create(null)
-redactors['class'] = function(redaction) {
-  const elements = document.getElementsByClassName(redaction.selector)
-  for (const e of elements) {
-    if (hide) {
-      redactElement(e, redaction.replacement)
-    } else {
-      unredactElement(e, redaction.replacement)
-    }
-  }
-}
-
-function redact() {
+const parsePage = () => {
   const redactions = redactionMap[document.domain]
   redactions.forEach(r => {
-    if (!(r.type in redactors)) {
-      throw new Error(`Unrecognized selector type: ${r.type}`)
+    const elements = document.querySelectorAll(r.selector)
+    for (const e of elements) {
+      if (hide) {
+        redactElement(e, r.replacement)
+      } else {
+        unredactElement(e, r.replacement)
+      }
     }
-    redactors[r.type](r)
   })
 }
 
 function unredactElement(element, replacementRule) {
-  // Don't touch re-redact elements we processed previously
-  if (element.getAttribute('data-processed') === hide) {
-    return
-  }
-
-  // Mark this element as having been processed.
-  element.setAttribute('data-processed', hide)
-
-  // Save the original value to allow for un-redaction
-  element.setAttribute('data-original-replacement-rule', replacementRule.type)
   if (replacementRule.type === 'text') {
-    element.textContent = element.getAttribute('data-original-value')
+    element.textContent = element.getAttribute('data-linkedincognito-textcontent')
+    element.removeAttribute('data-linkedincognito-textcontent')
   } else if (replacementRule.type === 'attribute') {
-    element.setAttribute(replacementRule.attribute, element.getAttribute('data-original-value'))
-  } else if (replacementRule.type === 'childElement-text') {
-    let innerElements = element.getElementsByTagName(replacementRule.childElementTag)
-    innerElements[0].textContent = innerElements[0].getAttribute('data-original-value')
-  } else if (replacementRule.type === 'childElement-attribute') {
-    let innerElements = element.getElementsByTagName(replacementRule.childElementTag)
-    innerElements[0].setAttribute(replacementRule.childElementAttribute, innerElements[0].getAttribute('data-original-value'))
+    const attributeKey = `data-linkedincognito-${replacementRule.attribute}`
+    element.setAttribute(replacementRule.attribute, element.getAttribute(attributeKey))
+    element.removeAttribute(attributeKey)
   } else {
     throw new Error(`Unrecognized replacment rule type ${replacementRule.type}`)
   }
 }
 
 function redactElement(element, replacementRule) {
-  // Don't touch re-redact elements we processed previously
-  // Note that hide is a boolean while getAttribute returns a string.
-  // if (element.getAttribute('data-processed') === ('' + hide)) {
-  //   return;
-  // }
-
-  // Mark this element as having been processed.
-  // element.setAttribute('data-processed', hide);
-
-  // Save the original value to allow for un-redaction
-  element.setAttribute('data-original-replacement-rule', replacementRule.type)
   if (replacementRule.type === 'text') {
-    if (element.getAttribute('data-original-value') === null) {
-      element.setAttribute('data-original-value', element.textContent)
+    const redactedTextContent = element.getAttribute('data-linkedincognito-textcontent')
+
+    if ([null, 'null', replacementRule.value].includes(redactedTextContent)) {
+      element.setAttribute('data-linkedincognito-textcontent', element.textContent)
     }
-    element.textContent = replacementRule.value
+
+    if (element.textContent !== replacementRule.value) {
+      element.textContent = replacementRule.value
+    }
   } else if (replacementRule.type === 'attribute') {
-    if (element.getAttribute('data-original-value') === null) {
-      element.setAttribute('data-original-value', element.getAttribute(replacementRule.attribute))
+    const attributeKey = `data-linkedincognito-${replacementRule.attribute}`
+    const redactedAttributeValue = element.getAttribute(attributeKey)
+
+    if ([null, 'null', replacementRule.value].includes(redactedAttributeValue)) {
+      element.setAttribute(attributeKey, element.getAttribute(replacementRule.attribute))
     }
-    element.setAttribute(replacementRule.attribute, replacementRule.value)
-  } else if (replacementRule.type === 'childElement-text') {
-    let innerElements = element.getElementsByTagName(replacementRule.childElementTag)
-    if (innerElements[0].getAttribute('data-original-value') === null) {
-      innerElements[0].setAttribute('data-original-value', innerElements[0].textContent)
+
+    // don't unnecessarily set the attribute, as to not set off a MutationObserver death spiral
+    if (element.getAttribute(replacementRule.attribute) !== replacementRule.value) {
+      element.setAttribute(replacementRule.attribute, replacementRule.value)
     }
-    innerElements[0].textContent = replacementRule.value
-  } else if (replacementRule.type === 'childElement-attribute') {
-    let innerElements = element.getElementsByTagName(replacementRule.childElementTag)
-    if (innerElements[0].getAttribute('data-original-value') === null
-      || innerElements[0].getAttribute('data-original-value') === 'null'
-      || innerElements[0].getAttribute('data-original-value') === replacementRule.value) {
-      innerElements[0].setAttribute('data-original-value', innerElements[0].getAttribute(replacementRule.childElementAttribute))
-      innerElements[0].setAttribute('data-original-attribute', replacementRule.childElementAttribute)
-    }
-    innerElements[0].setAttribute(replacementRule.childElementAttribute, replacementRule.value)
   } else {
     throw new Error(`Unrecognized replacment rule type ${replacementRule.type}`)
   }
@@ -101,14 +66,23 @@ function redactElement(element, replacementRule) {
 
 function observe() {
   // select the target node
-  var target = document.getElementsByTagName('body')[0]
+  var [target] = document.getElementsByTagName('body')
 
   // create an observer instance
   var observer = new MutationObserver(() => {
-    redact()
+    if (hide) {
+      parsePage()
+    }
   })
-  observer.observe(target, { childList: true, subtree: true })
+  observer.observe(target, {
+    childList: true,
+    subtree: true,
+    // if an img's src is lazy-loaded we won't always catch the change with a childList update, so we have to listen for an attribute change
+    // currently limited to `src` but may need to include attributes like `style` (for background-image) going forward
+    attributes: true,
+    attributeFilter: ['src']
+  })
 }
 
-redact()
+parsePage()
 observe()
--- redactionMap.js
+++ redactionMap.js
@@ -1,16 +1,15 @@
 const redactionMap = {
   'www.linkedin.com': [
     {
-      type: 'class',
-      selector: 'pv-top-card-section__name Sans-26px-black-85%',
+      description: 'Main candidate profile name',
+      selector: '.pv-top-card-section__name',
       replacement: {
         type: 'text',
         value: '[Name]'
       }
     },
     {
-      type: 'class',
-      selector: 'pv-top-card-section__image',
+      selector: '.pv-top-card-section__image',
       replacement: {
         type: 'attribute',
         attribute: 'src',
@@ -19,19 +18,16 @@ const redactionMap = {
     },
     {
       description: 'People Also Viewed...',
-      type: 'class',
-      selector: 'pv-browsemap-section__member-container mt4',
+      selector: '.pv-browsemap-section__member-container.mt4 img',
       replacement: {
-        type: 'childElement-attribute',
-        childElementTag: 'img',
-        childElementAttribute: 'src',
+        type: 'attribute',
+        attribute: 'src',
         value: '/favicon.ico'
       }
     },
     {
       description: 'own profile avatar',
-      type: 'class',
-      selector: 'profile-photo-edit__preview',
+      selector: '.profile-photo-edit__preview',
       replacement: {
         type: 'attribute',
         attribute: 'src',
@@ -40,8 +36,7 @@ const redactionMap = {
     },
     {
       description: 'Names in Search Results on https://www.linkedin.com/search/results/index/',
-      type: 'class',
-      selector: 'name actor-name',
+      selector: '.name.actor-name',
       replacement: {
         type: 'text',
         value: '[Name]'
@@ -49,77 +44,63 @@ const redactionMap = {
     },
     {
       description: 'Photos in Search Results on https://www.linkedin.com/search/results/index/',
-      type: 'class',
-      selector: 'search-result__image',
+      selector: '.search-result__image img',
       replacement: {
-        type: 'childElement-attribute',
-        childElementTag: 'img',
-        childElementAttribute: 'src',
+        type: 'attribute',
+        attribute: 'src',
         value: '/favicon.ico'
       }
     }
   ],
   'angel.co': [
     {
-      type: 'class',
-      selector: 'title u-fontSize14 u-colorGray3',
+      selector: '.title.u-fontSize14.u-colorGray3', // shouldn't use a "styling" class to identify this element
       replacement: {
         type: 'text',
         value: '[Candidate Name]'
       }
     },
     {
-      type: 'class',
-      selector: 'photo s-vgRight0_5',
+      selector: '.photo.s-vgRight0_5 img',
       replacement: {
-        type: 'childElement-attribute',
-        childElementTag: 'img',
-        childElementAttribute: 'src',
+        type: 'attribute',
+        attribute: 'src',
         value: 'https://angel.co/images/shared/nopic.png'
       },
     },
     {
-      type: 'class',
-      selector: 's-grid-colSm24 u-colorGray3',
+      selector: '.s-grid-colSm24.u-colorGray3', // shouldn't use a "styling" class to identify this element
       replacement: {
         type: 'text',
         value: 'Intro Note'
       }
     },
     {
-      type: 'class',
-      selector: 'name u-fontSize24 u-fontWeight300 section-title',
+      selector: '.name.u-fontSize24.u-fontWeight300.section-title a', // shouldn't use a "styling" class to identify this element
       replacement: {
-        type: 'childElement-text',
-        childElementTag: 'a',
+        type: 'text',
         value: '[Candidate Name]'
       }
     },
     {
-      type: 'class',
-      selector: 'field-row-val s-flexgrid-colSmW s-vgPadLeft1',
+      selector: '.field-row-val.s-flexgrid-colSmW.s-vgPadLeft1 a',
       replacement: {
-        type: 'childElement-text',
-        childElementTag: 'a',
+        type: 'text',
         value: '[Candidate Email]'
       }
     },
     {
-      type: 'class',
-      selector: 'js-browse-table-row-name s-vgRight0_5 candidate-name u-floatLeft',
+      selector: '.js-browse-table-row-name.s-vgRight0_5.candidate-name.u-floatLeft a',
       replacement: {
-        type: 'childElement-text',
-        childElementTag: 'a',
+        type: 'text',
         value: '[Candidate Name]'
       }
     },
     {
-      type: 'class',
-      selector: 'photo js-add-photo',
+      selector: '.photo.js-add-photo img',
       replacement: {
-        type: 'childElement-attribute',
-        childElementTag: 'img',
-        childElementAttribute: 'src',
+        type: 'attribute',
+        attribute: 'src',
         value: 'https://angel.co/images/shared/nopic.png'
       },
     },
